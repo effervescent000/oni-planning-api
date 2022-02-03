dbdfa6cbadb9a9b8f15f8be19d778434df77919a
effervescent000 <kislio01@gmail.com>
2022-02-03 07:10:47 -0500

    auth skeleton

---
+++ oni_calcs_api/auth.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, request, jsonify, current_app
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    set_access_cookies,
+    get_jwt,
+    unset_jwt_cookies,
+    jwt_required,
+    current_user,
+)
+
+from .models import User
+from .schema import UserSchema
+
+from . import db, jwt
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+one_user_schema = UserSchema()
+multi_user_schema = UserSchema(many=True)
+
+# GET endpoints
+
+# POST endpoints
+
+
+@bp.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if User.query.filter_by(username=username).first() == None:
+        new_user = User(username=username, password=User.hash_password(password))
+        db.session.add(new_user)
+        db.session.commit()
+        response = jsonify(one_user_schema.dump(new_user))
+        access_token = create_access_token(identity=username)
+        set_access_cookies(response, access_token)
+        return response
+    return jsonify("User already exists"), 401
+
+
+@bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    user_query = User.query.filter_by(username=username).first()
+    if user_query == None:
+        return jsonify("Invalid username/password"), 401
+    if not user_query.check_password(password):
+        return jsonify("Invalid username/password"), 401
+    response = jsonify(one_user_schema.dump(user_query))
+    access_token = create_access_token(identity=username)
+    set_access_cookies(response, access_token)
+    return response
+
+
+@bp.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response, 200
+
+
+# PUT endpoints
+
+# DELETE endpoints
+
+# utils
